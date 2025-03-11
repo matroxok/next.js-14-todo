0bff7b3c4977e0d7e2d1fdcda2903c0699328ae6
Mateusz Kozera <mateusz.kozera.wroc@gmail.com>
2025-03-11 01:08:08 +0100

    parms object

--- app/[username]/[taskId]/gh/page.tsx
+++ app/[username]/[taskId]/gh/page.tsx
@@ -15,12 +15,9 @@ const getRepoMetadata = async (taskId: string) => {
 export default async function RepoMetaPage({
   params,
 }: {
-  // @ts-ignore
-  params: { username: string; taskId: string } | Promise<{ username: string; taskId: string }>;
+  params: { username: string; taskId: string };
 }) {
-  // "Awaitujemy" params, aby dostać zwykły obiekt
-  const resolvedParams = await params;
-  const meta = await getRepoMetadata(resolvedParams.taskId);
+  const meta = await getRepoMetadata(params.taskId);
 
   return <pre>{JSON.stringify(meta, null, 2)}</pre>;
 }
